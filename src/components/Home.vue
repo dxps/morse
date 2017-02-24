eda66277b7b922fae2163f39e57486a1d6de04d9
visvadw <visvadw@gmail.com>
2017-02-24 22:48:58 +0200

    Back button with programmatic navigation

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,26 @@
 <template>
 <div class="hello">
    <h3>Home</h3>
-   <el-button>Default Button</el-button>
+   <el-button @click="this.gotoHome">Back</el-button>
 </div>
 </template>
 
 <script>
+import router from '../router';
+
 export default {
    name: 'hello',
    data() {
       return {
          msg: '',
       };
+   },
+   methods: {
+      gotoHome() {
+         // eslint-disable-next-line
+         // console.console.log('gotoHome');
+         router.push('/');
+      },
    },
 };
 </script>
