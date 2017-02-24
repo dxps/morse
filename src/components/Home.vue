8ad5f71a327e8ef2c3bcf5d08a4f0e9d9a96c53b
visvadw <visvadw@gmail.com>
2017-02-24 22:19:36 +0200

    1st try element-ui + router with history (no more hash)

---
+++ src/components/Home.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="hello">
+   <h3>Home</h3>
+   <el-button>Default Button</el-button>
+</div>
+</template>
+
+<script>
+export default {
+   name: 'hello',
+   data() {
+      return {
+         msg: '',
+      };
+   },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+   font-weight: normal;
+}
+
+ul {
+   list-style-type: none;
+   padding: 0;
+}
+
+li {
+   display: inline-block;
+   margin: 0 10px;
+}
+
+a {
+   color: #42b983;
+}
+</style>
